5d40ee8735ecd1b0d3dfc397befd4cadddf24edf
Amisha mohanty <amishamohanty2025@gmail.com>
2025-08-13 23:27:37 +0530

    Add files via upload

---
+++ Practise/patterns.java
@@ -0,0 +1,119 @@
+public class patterns {
+    public static void main(String[] args) {
+    //Pattern 1 
+      for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+      }
+
+    // Pattern 2
+    for(int i=0;i<=5;i++){
+        for(int j=0;j<i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+      }
+    // Pattern 3
+    for(int i=1;i<=5;i++){
+      for(int j=1;j<=i;j++){
+          System.out.print(j+" ");
+      }
+      System.out.println();
+    }
+
+    // Pattern 4
+    for(int i=1;i<=5;i++){
+      for(int j=1;j<=i;j++){
+          System.out.print(i+" ");
+      }
+      System.out.println();
+    } 
+
+     // Pattern 5
+     for(int i=1;i<=5;i++){
+      for(int j=0;j<5-i+1;j++){
+          System.out.print("* ");
+      }
+      System.out.println();
+    } 
+
+    // Pattern 6
+    for(int i=1;i<=5;i++){
+      for(int j=1;j<=5-i+1;j++){
+          System.out.print(j+" ");
+      }
+      System.out.println();
+    } 
+
+    // Pattern 7
+    for(int i=0;i<5;i++){
+      // for space
+      for(int j=0;j<5-i-1;j++){
+          System.out.print(" ");
+      }
+      // for star
+      for (int j=0;j<(2*i+1);j++) {
+          System.out.print("*");
+      }
+       // for space
+       for(int j=0;j<5-i-1;j++){
+        System.out.print(" ");
+    }
+      System.out.println();
+    } 
+
+    // Pattern 8
+    for(int i=0;i<5;i++){
+      // for space
+      for(int j=0;j<i;j++){
+          System.out.print(" ");
+      }
+      // for star
+      for (int j=0;j<(10-(2*i+1));j++) {
+          System.out.print("*");
+      }
+       // for space
+       for(int j=0;j<i;j++){
+        System.out.print(" ");
+    }
+      System.out.println();
+    } 
+
+    // Pattern 9(not done doubt)
+    for(int i=0;i<5;i++){
+      // for space
+      for(int j=0;j<5-i-1;j++){
+          System.out.print(" ");
+      }
+      // for star
+      for (int j=0;j<(2*i+1);j++) {
+          System.out.print("*");
+      }
+       // for space
+       for(int j=0;j<5-i-1;j++){
+        System.out.print(" ");
+      }
+       for(int j=0;j<i;j++){
+        System.out.print(" ");
+      }
+      // for star
+       for (int j=0;j<(10-(2*i+1));j++) {
+        System.out.print("*");
+      }
+      // for space
+       for(int j=0;j<i;j++){
+        System.out.print(" ");
+      }
+      System.out.println();
+    } 
+
+    // Pattern 10
+    for (int i = 1;i<=5;i++){
+      for (int j=1;j<=i;j++){
+        System.out.println("* ");
+      }
+    }
+  }
+}
